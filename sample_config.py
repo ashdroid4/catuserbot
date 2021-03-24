d2128b7f719f3da17526ee640ec1ff898a0f658b
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-03-24 19:00:00 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -109,7 +109,7 @@ class Config(object):
     NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
 
     # in alive message pic
-    ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
+    ALIVE_PIC_1 = os.environ.get("ALIVE_PIC_1", None)
     ALIVE_PIC_2 = os.environ.get("ALIVE_PIC_2", None)
     ALIVE_PIC_3 = os.environ.get("ALIVE_PIC_3", None)
     ALIVE_PIC_4 = os.environ.get("ALIVE_PIC_4", None)
